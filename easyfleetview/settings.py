b048725e55d95dba45e00b3d254ad661276228f9
Jared Tangir <jaredtangir@Jareds-MacBook-Pro-2.local>
2019-07-22 18:09:00 -0400

    updated detail template layouts

--- easyfleetview/settings.py
+++ easyfleetview/settings.py
@@ -157,7 +157,7 @@ DATABASE_ROUTERS = (
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 STATIC_ROOT= os.path.join(BASE_DIR, 'static')
-STATICFILES_DIRS = '/static/',
+STATICFILES_DIRS = 'static',
 STATIC_URL = '/static/'
 
 LOGIN_REDIRECT_URL = 'dashboard'
